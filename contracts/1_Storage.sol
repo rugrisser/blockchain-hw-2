f0ec8faec2fd04b0af94ccf60cd29c03283f0f94
Vyacheslav Ryzhkov <rugrisser@gmail.com>
2023-11-17 03:10:00 +0300

    Functions

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -21,6 +21,26 @@ contract Storage {
     event VehicleRegistred(string vin, address owner, string plateNumber);
     event VehicleDeregistred(string vin);
 
+    function registerVehicle(string memory _vin, string memory _plateNumber) public {
+        require(
+            vehicleLicenses[_vin].owner == address(0x0), 
+            "License for given VIN already exists"
+        );
+
+        vehicleLicenses[_vin] = VehicleLicense(_vin, msg.sender, _plateNumber);
+        emit VehicleRegistred(_vin, msg.sender, _plateNumber);
+    }
+
+    function deregisterVehicle(string memory _vin) public {
+        require(
+            vehicleLicenses[_vin].owner == msg.sender,
+            "License with given VIN is not exist or assigned to another owner"
+        );
+
+        delete vehicleLicenses[_vin];
+        emit VehicleDeregistred(_vin);
+    }
+
     /**
      * @dev Store value in variable
      * @param num value to store
